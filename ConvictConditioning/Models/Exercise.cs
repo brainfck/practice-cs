5b50ca6e79ba00fd59394ead63b23d40aa09bbf8
brainfck <mykhailo.tsuber@gmail.com>
2023-12-20 14:41:28 +0100

    initial changes

---
+++ ConvictConditioning/Models/Exercise.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConvictConditioning.Models;
+
+public class Exercise {
+  [Required]
+  public int Id { get; set; }
+  [Required]
+  public string? Title {  get; set; }
+  [Required]
+  public ExerciseType Type { get; set; } = ExerciseType.None;
+
+  [Required]
+  [Range(1, 10, ErrorMessage = "The Level must be between 1 and 10.")]
+  public int Level { get; set; }
+
+  [Required]
+  public string ProgressOne { get; set; } = string.Empty;
+  [Required]
+  public string ProgressTwo { get; set; } = string.Empty;
+  [Required]
+  public string ProgressThree { get; set; } = string.Empty;
+}
+
+public enum ExerciseType {
+    None,
+    Pushups,
+    Squats,
+    Pullups,
+    Raises,
+    Bridges,
+    HandstandPushups
+}
